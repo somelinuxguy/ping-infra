755af7397fef38d3bdc8df7ed29b6ceabda2a317
Nathan Reese <github@sect.net>
2023-09-15 08:13:09 -0400

    move ingress to a subdir so we can do two runs more cleanly

--- main.tf
+++ main.tf
@@ -1,7 +1,1 @@
-module "ingress" {
-  source     = "./modules/k8s"
-  oidc_arn   = module.eks.oidc_provider_arn
-  account_id = data.aws_caller_identity.current.account_id
-  region     = var.region
-  depends_on = [module.eks]
-}
+# Nothing to see here.check
