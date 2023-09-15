61cb2b1ccfeccdc4a6075a9ddd55201e04fa8990
Nathan Reese <github@sect.net>
2023-09-15 08:16:07 -0400

    run.sh to give us a clean entry point for some of terraforms oddities

---
+++ run.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+I'm a script that will weave together the required steps to apply this infrastructure for you.
+
+I'm WIP so I wont work yet.
+
+set up the env correctly
+apply the main directory, wait for it to run
+apply the ingress controller
