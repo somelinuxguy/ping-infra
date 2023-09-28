7480a4aae4460a0ce909ebd4354b1fbdd9b4ac6f
Nathan Reese <github@sect.net>
2023-09-28 13:34:30 -0400

    validation example

--- acm.tf
+++ acm.tf
@@ -11,3 +11,34 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
+
+# --
+# Right now, I use NameCheap but I'm placing this here from the terraform docs
+# for easy reference.
+# --
+# data "aws_route53_zone" "sect" {
+#   name         = "sect.net"
+#   private_zone = false
+# }
+
+# resource "aws_route53_record" "sect" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.sect.zone_id
+# }
+
+# resource "aws_acm_certificate_validation" "sect" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.sect : record.fqdn]
+# }
